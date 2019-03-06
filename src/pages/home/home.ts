e759354973d136a4e1b2f9f08f5e388108316c04
Linazzy <36626979+Linazzy@users.noreply.github.com>
2019-03-06 11:57:11 +0100

    principio

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,68 @@
+import { Camera } from '@ionic-native/camera';
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
-
+import { NavController, ActionSheetController } from 'ionic-angular';
+import { NgProgress } from '@ngx-progressbar/core';
+import * as Tesseract from 'tesseract.js';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  selectedImage: string;
+  imageText: string;
+
+  constructor(public navCtrl: NavController, private actionSheetCtrl: ActionSheetController,
+    private camera: Camera, public progress: NgProgress) {
 
   }
-
-}
+  selectSource() {    
+    let actionSheet = this.actionSheetCtrl.create({
+      buttons: [
+        {
+          text: 'Use Library',
+          handler: () => {
+            this.getPicture(this.camera.PictureSourceType.PHOTOLIBRARY);
+          }
+        }, {
+          text: 'Capture Image',
+          handler: () => {
+            this.getPicture(this.camera.PictureSourceType.CAMERA);
+          }
+        }, {
+          text: 'Cancel',
+          role: 'cancel'
+        }
+      ]
+    });
+    actionSheet.present();
+  }
+ 
+  getPicture(sourceType: PictureSourceType) {
+    this.camera.getPicture({
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      sourceType: sourceType,
+      allowEdit: true,
+      saveToPhotoAlbum: false,
+      correctOrientation: true
+    }).then((imageData) => {
+      this.selectedImage = `data:image/jpeg;base64,${imageData}`;
+    });
+  }
+ 
+  recognizeImage() {
+    Tesseract.recognize(this.selectedImage)
+    .progress(message => {
+      if (message.status === 'recognizing text')
+      this.progress.set(message.progress);
+    })
+    .catch(err => console.error(err))
+    .then(result => {
+      this.imageText = result.text;
+    })
+    .finally(resultOrError => {
+      this.progress.complete();
+    });
+  }
+  }
